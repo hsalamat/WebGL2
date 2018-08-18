a306ffd6161169253add10e9bc499d85d1612e3d
hsalamat <hooman_salamat@yahoo.com>
2018-08-18 15:04:50 -0400

    Add files via upload

---
+++ Appendix A/appendixA-main.js
@@ -0,0 +1,7 @@
+// appendixA-main.js
+import { hello,repeat,MyFunction } from './appendixA-mymodule.js';
+
+let val = hello(); // val is "Hello";
+alert(val);
+console.log(repeat('WebGL'));
+console.log(MyFunction('WebGL in Action'));
---
+++ Appendix A/appendixA-mymodule.js
@@ -0,0 +1,8 @@
+//appendixA-mymodule.js
+export function hello() {
+    return "Hello World";
+}
+export const repeat = (string) => `${string} ${string}`;
+export function MyFunction(string) {
+    return `${string}!`;
+}
