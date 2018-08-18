4987da2363c1e1e12de1a12edc4d1fafa9e74ca4
Hooman <hooman_salamat@yahoo.com>
2018-08-18 15:04:12 -0400

    AppendixA

---
+++ Appendix A/appendixA-speedtest.js
@@ -0,0 +1,8 @@
+﻿
+console.log("Testing the speed...");
+        var speed = 20;
+        function accelerate(arg) {
+            arg++;
+            return arg;
+        }
+console.log("speed is " + accelerate(speed));
