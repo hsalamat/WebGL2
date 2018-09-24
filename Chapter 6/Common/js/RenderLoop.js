a870e5f96c38fddd803c01c4d2cca21432934484
Hooman <hooman_salamat@yahoo.com>
2018-09-24 13:17:44 -0400

    updated

--- Chapter 6/Common/js/RenderLoop.js
+++ Chapter 6/Common/js/RenderLoop.js
@@ -10,14 +10,14 @@ rloop = new RenderLoop(function(dt){
 class RenderLoop{
 	constructor(callback,fps){
 		var oThis = this;
-		this.msLastFrame = null;	//The time in Miliseconds of the last frame.
+		this.msLastFrame = null;	//The time in Milliseconds of the last frame.
 		this.callBack = callback;	//What function to call for each frame
 		this.isActive = false;		//Control the On/Off state of the render loop
 		this.fps = 0;				//Save the value of how fast the loop is going.
 
         //if (!fps && fps > 0)
 		if(fps > 0){ //Build a run method that limits the framerate
-			this.msFpsLimit = 1000/fps; //Calc how many milliseconds per frame in one second of time.
+			this.msFpsLimit = 1000/fps; //Calculate how many milliseconds per frame in one second of time.
 			
 			this.run = function(){
 				//Calculate Deltatime between frames and the FPS currently.
@@ -36,8 +36,8 @@ class RenderLoop{
 		}else{ //Else build a run method thats optimised as much as possible.
 			this.run = function(){
 				//Calculate Deltatime between frames and the FPS currently.
-				var msCurrent	= performance.now(),	//Gives you the whole number of how many milliseconds since the dawn of time :)
-					deltaTime	= (msCurrent - oThis.msLastFrame) / 1000.0;	//ms between frames, Then / by 1 second to get the fraction of a second.
+				var msCurrent	= performance.now(),	//Gives you the whole number of how many milliseconds since the begining
+					deltaTime	= (msCurrent - oThis.msLastFrame) / 1000.0;	//ms between frames, by 1 second to get the fraction of a second.
 
 				//Now execute frame since the time has elapsed.
 				oThis.fps			= Math.floor(1/deltaTime); //Time it took to generate one frame, divide 1 by that to get how many frames in one second.
